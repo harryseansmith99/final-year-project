bf3f937371d03051b359822ec41db914b5d1ca4a
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-10 11:33:18 +0100

    working out how to display results in table from db on page

--- products.php
+++ products.php
@@ -9,6 +9,20 @@
 
     <h1>get all products table test</h1>
 
+    <table>
+        <tr>
+            <th>productID</th>
+            <th>categoryID<_fk/th>
+            <th>categoryName</th>
+            <th>productName</th>
+            <th>productDescription</th>
+            <th>productSerialNumber</th>
+            <th>quantity</th>
+            <th>minimumStockLevel</th>
+            <th>maximumStockLevel</th>
+            <th>locationName</th>
+            <th>locationAddress</th>
+
     <?php
 
     // connection settings
@@ -26,13 +40,50 @@
     }
 
     // call procedure
-    $allProductsProcedure = "CALL proc_getAllProducts()";
-    $query = mysqli_query($conn, $getAllProductsProcedure);
+    // $query = "CALL proc_getAllProducts();";
 
-    
+    $sql = "SELECT
+        ProductTable.productID, 
+        ProductTable.categoryID_fk, 
+        CategoryTable.categoryName,
+        ProductTable.productName,
+        ProductTable.productDescription,
+        ProductTable.productSerialNumber,
+        StockTable.quantity,
+        StockTable.minimumStockLevel,
+        StockTable.maximumStockLevel,
+        LocationTable.locationName,
+        LocationTable.locationAddress
+    FROM ProductTable
+        JOIN CategoryTable ON ProductTable.categoryID_fk = CategoryTable.categoryID
+        JOIN StockTable ON StockTable.productID_fk = ProductTable.productID
+        JOIN LocationTable ON StockTable.locationID_fk = LocationTable.locationID;";
+
+
+    $result = $conn->query($sql);
 
 
 
-?>
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["productID"] . "</td><td>" . $row["categoryID_fk"] . "</td><td>"
+            . $row["categoryName"] . "</td><td>" . $row["productName"] . "</td><td>" .
+            $row["productDescription"] . "</td><td>" . $row["productSerialNumber"] . 
+            "</td><td>" . $row["quantity"] . "</td><td>" . $row["minimumStockLevel"] . 
+            "</td><td>" . $row["maximumStockLevel"] . "</td><td>" . $row["locationName"] . 
+            "</td><td>" . $row["locationAddress"] . "</td><tr>";
+        }
+        echo "</table>";
+    }
+    else {
+        echo "no results";
+    }
+
+
+    $conn->close();
+
+    ?>
+    </table>
+
 </body>
 </html>
