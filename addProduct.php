53c6d7f11c845b507131baf3ea7e68a8f06bbefb
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-12 12:05:03 +0100

    template for add product page done

--- addProduct.php
+++ addProduct.php
@@ -16,10 +16,68 @@
         <br><br>
         <form method="post">
             <div class="row mb-3">
-                <select name="select_box" class="form-select" id="select_box">
+                <select name="select_box" class="form-select form-select-md" id="select_box">
                     <option value="">Select Category</option>
                     <?php include "includes/getCategories.php"; ?>
                 </select>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Product Name</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="newProductName" value="">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Product Description</label>
+                <br>
+                <div class="col-sm-6">
+                    <textarea class="form-control" name="newProductDescription"></textarea>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Serial Number</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="newSerialNumber" value="">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Storage Location</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="storageLocationToAdd" value="">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Received Quanity</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="receivedQuantity" value="">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Minimum Quantity</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="possibleMinimumQuantity" value="">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm col-form-label">Minimum Quantity</label>
+                <br>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="possibleMaximumQuantity" value="">
+                </div>
+            </div>
+            <br><br>
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-outline-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-danger" href="products.php" role="button">Cancel</a>
+                </div>
             </div>
         </form>
     </div>
