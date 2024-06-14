97eaa966c5e401943c4ca3c3d751f8e77a407b15
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-14 17:53:48 +0100

    added radio buttons

--- alterStock.php
+++ alterStock.php
@@ -1,5 +1,5 @@
 <?php 
-include "includes/postNewProduct.php";
+//include "includes/postNewProduct.php";
 ?>
 
 <!DOCTYPE html>
@@ -36,60 +36,27 @@ include "includes/postNewProduct.php";
                 ?>
                 <form action="alterStock.php" method="post">
                     <div class="row mb-3">
-                        <label >Cateogry</label>
-                        <select name="categorySelect" class="form-select form-select-md">
-                            <option value="<?php echo $categorySelect;?>">Select Category</option>
+                        <label>Product</label>
+                        <select name="productSelect" class="form-select form-select-md">
+                            <option value="<?php echo $productSelect;?>">Select Product</option>
                             <?php include "includes/getCategories.php"; ?>
                         </select>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Name</label>
+                        <label class="col-sm col-form-label">Amount</label>
                         <br>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newProductName" value="<?php echo $newProductName;?>">
+                            <input type="text" class="form-control" name="amount" value="<?php echo $amount;?>">
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Description</label>
+                        <label class="col-sm col-form-label">Book In
+                            <input type="radio" name="incOrDec" value="bookIn">
+                        </label>
+                        <label class="col-sm col-form-label">Book Out
+                            <input type="radio" name="incOrDec" value="bookOut">
+                        </label>
                         <br>
-                        <div class="col-sm-6">
-                            <textarea class="form-control" name="newProductDescription" value="<?php echo $newProductDescription;?>"></textarea>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Serial Number</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newSerialNumber" value="<?php echo $newSerialNumber;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Storage Location</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="storageLocationToAdd" value="<?php echo $storageLocationToAdd;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Received Quanity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="receivedQuantity" value="<?php echo $receivedQuantity;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Minimum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMinimumQuantity" value="<?php echo $possibleMinumumQuantity;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Minimum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMaximumQuantity" value="<?php echo $possibleMaximumQuantity;?>">
-                        </div>
                     </div>
                     <div class="row mb-3">
                         <div class="offset-sm-3 col-sm-3 d-grid">
