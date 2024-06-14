8d53a16b936efaec393bbebae3e2d8626a0acced
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-14 14:50:52 +0100

    switched order of sidebar links

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,9 +4,9 @@ https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
 
 <div class="sidenav">
   <a href="products.php">Products</a>
-  <a href="#Users">Users</a>
-  <a href="#Book In">Book In</a>
+  <a href="#alterStock">Alter Stock/a>
   <a href="#Book Out">Book Out</a>
+  <a href="#Users">Users</a>
   <a href="#Logout">Logout</a>
   
 </div>
