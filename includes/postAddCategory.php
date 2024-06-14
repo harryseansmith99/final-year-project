6ef6bf5a8359a06be735ab171717ed77ffe4f7c7
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-14 12:17:14 +0100

    added input form to submit new category name

---
+++ includes/postAddCategory.php
@@ -0,0 +1,37 @@
+<?php
+
+include "includes/connectionSettings.php";
+
+// init empty variables as place holders
+
+$newCategoryName = "";
+
+$errorMessage = "";
+$successMessageProduct = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $newCategoryName = "";
+
+    // do while false allows this to break out after finished
+    do {
+        if (empty($newCategoryName)) {
+            $errorMessage = "Category Name Field Required";
+            break;
+        }
+
+        // prepare and bind
+        $sql = $conn->prepare("CALL proc_addNewCategory(?)");
+        $sql->bind_param("s", $newCategoryName);
+
+        if (! $sql->execute()) {
+            $errorMessage = "Query is not valid: " . $conn->error;
+            break;
+        }
+        else {
+            $successMessageProduct = "successfully added product";
+        }
+
+    } while(false);
+} 
+
+?>
