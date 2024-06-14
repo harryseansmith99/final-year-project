501d7e8e2bddc372865285c18a9a8456915a4681
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-14 15:03:14 +0100

    fixed location not showing on edit in <textarea> tags

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,8 +4,7 @@ https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
 
 <div class="sidenav">
   <a href="products.php">Products</a>
-  <a href="#alterStock">Alter Stock/a>
-  <a href="#Book Out">Book Out</a>
+  <a href="#Alter Stock">Alter Stock</a>
   <a href="#Users">Users</a>
   <a href="#Logout">Logout</a>
   
