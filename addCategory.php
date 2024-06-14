161ed4602bc79395faeac9632b68e55c72f54435
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-14 12:12:48 +0100

    form input for new cat name, delete every other entry except submit and cancel

--- addCategory.php
+++ addCategory.php
@@ -34,64 +34,14 @@
                     ";
                 }
                 ?>
-                <form action="addProduct.php" method="post">
+                <form action="addCategory.php" method="post">
                     <div class="row mb-3">
-                        <label >Cateogry</label>
-                        <select name="categorySelect" class="form-select form-select-md">
-                            <option value="<?php echo $categorySelect;?>">Select Category</option>
-                            <?php include "includes/getCategories.php"; ?>
-                        </select>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Name</label>
+                        <label class="col-sm col-form-label">New Category Name</label>
                         <br>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newProductName" value="<?php echo $newProductName;?>">
+                            <input type="text" class="form-control" name="newCategoryName" value="<?php echo $newCategoryName;?>">
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Description</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <textarea class="form-control" name="newProductDescription" value="<?php echo $newProductDescription;?>"></textarea>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Serial Number</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newSerialNumber" value="<?php echo $newSerialNumber;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Storage Location</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="storageLocationToAdd" value="<?php echo $storageLocationToAdd;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Received Quanity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="receivedQuantity" value="<?php echo $receivedQuantity;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Minimum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMinimumQuantity" value="<?php echo $possibleMinumumQuantity;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <label class="col-sm col-form-label">Minimum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMaximumQuantity" value="<?php echo $possibleMaximumQuantity;?>">
-                        </div>
-                    </div>
-                    <div class="row mb-3">
                         <div class="offset-sm-3 col-sm-3 d-grid">
                             <button type="submit" class="btn btn-outline-primary">Submit</button>
                         </div>
