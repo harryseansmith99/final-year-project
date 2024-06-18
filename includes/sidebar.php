0454ca61c673f2ff6d9bbb6f0828aeab653d7f6c
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-18 20:07:20 +0100

    changed sidebar with proper access per user type

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,16 +4,22 @@ https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
 
 <?php 
 
-session_start();
+include "includes/authenticateAdmin.php";
 
-
-
-?>
+echo '
 
 <div class="sidenav">
   <a href="products.php">Products</a>
-  <a href="alterStock.php">Alter Stock</a>
-  <a href="users.php">Users</a>
-  <a href="#Logout">Logout</a>
+  <a href="alterStock.php">Alter Stock</a>';
+  // only admin should have users page
+  if ($isAdmin) {
+    echo '<a href="users.php">Users</a>';
+  }
+  // rest of sidebar
+  echo '
+    <a href="#Logout">Logout</a>
 </div>
+  ';
+
+?>
 
