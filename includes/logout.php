8c4ce900a690b8cd5100af85ee12d722d919f1cf
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-18 20:24:05 +0100

    MVP completed from here

--- includes/logout.php
+++ includes/logout.php
@@ -1,10 +1,8 @@
 <?php
-
 // this will effectively log out the user
 session_start();
 session_unset();
 session_destroy();
-
 // take them back to homepage
 header("Location: ../index.php");
 exit;
