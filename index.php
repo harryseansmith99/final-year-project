e2ecdba31fdb351590b1c26090c28b711c1fdd60
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-18 12:28:55 +0100

    added login template (index.php)

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <title>Users</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css?<?php echo time(); ?>">
 </head>
-<body class="">
-    
+<body class="login-form-container">
+    <form action="/action_page.php" method="post">
+        <label for="email"><b>Email</b></label>
+        <input class="email-input" type="text" placeholder="Enter Email" name="email" required>
+        <br><br>
+
+        <label for="password"><b>Password</b></label>
+        <input class="password-input" type="password" placeholder="Enter Password" name="password" required>
+        <br><br>
+        <button class="login-button" type="submit">Login</button>
+    </form>
 </body>
+</html>
