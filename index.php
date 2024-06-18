3f0fe7e6865eeed1f1ef11793a733676687a4f1b
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-18 12:31:28 +0100

    centered login form on page

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
+    <title>Login</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css?<?php echo time(); ?>">
 </head>
-<body class="login-form-container">
-    <form action="/action_page.php" method="post">
-        <label for="email"><b>Email</b></label>
-        <input class="email-input" type="text" placeholder="Enter Email" name="email" required>
-        <br><br>
-
-        <label for="password"><b>Password</b></label>
-        <input class="password-input" type="password" placeholder="Enter Password" name="password" required>
-        <br><br>
-        <button class="login-button" type="submit">Login</button>
-    </form>
+<body class="login-body">
+    <div class="login-form-container">
+        <form action="/action_page.php" method="post">
+            <label for="email"><b>Email</b></label>
+            <input class="email-input" type="text" placeholder="Enter Email" name="email" required>
+            <br><br>
+            <label for="password"><b>Password</b></label>
+            <input class="password-input" type="password" placeholder="Enter Password" name="password" required>
+            <br><br>
+            <button class="login-button" type="submit">Login</button>
+        </form>
+    </div>
 </body>
 </html>
