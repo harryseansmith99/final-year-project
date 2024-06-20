60b0b0b4ed5a2246830d87051a00ab9c1754d738
harryseansmith99 <harryseansmith99@gmail.com>
2024-06-20 15:09:11 +0100

    adding future css for js validation

--- addProduct.php
+++ addProduct.php
@@ -43,52 +43,72 @@ include "includes/postAddProduct.php";
                         </select>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Name</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newProductName" value="<?php echo $newProductName;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Product Name</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="newProductName" name="newProductName" value="<?php echo $newProductName;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Product Description</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <textarea class="form-control" name="newProductDescription" value="<?php echo $newProductDescription;?>"></textarea>
+                        <div>
+                            <label class="col-sm col-form-label">Product Description</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <textarea class="form-control" id="newProductDescription" name="newProductDescription" value="<?php echo $newProductDescription;?>"></textarea>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Serial Number</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="newSerialNumber" value="<?php echo $newSerialNumber;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Serial Number</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="newSerialNumber" name="newSerialNumber" value="<?php echo $newSerialNumber;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Storage Location</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="storageLocationToAdd" value="<?php echo $storageLocationToAdd;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Storage Location</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="storageLocationToAdd" name="storageLocationToAdd" value="<?php echo $storageLocationToAdd;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Received Quanity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="receivedQuantity" value="<?php echo $receivedQuantity;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Received Quanity</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="receivedQuantity" name="receivedQuantity" value="<?php echo $receivedQuantity;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Minimum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMinimumQuantity" value="<?php echo $possibleMinumumQuantity;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Minimum Quantity</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="possibleMinimumQuantity" name="possibleMinimumQuantity" value="<?php echo $possibleMinumumQuantity;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label class="col-sm col-form-label">Maximum Quantity</label>
-                        <br>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="possibleMaximumQuantity" value="<?php echo $possibleMaximumQuantity;?>">
+                        <div class="input-control">
+                            <label class="col-sm col-form-label">Maximum Quantity</label>
+                            <br>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="possibleMaximumQuantity" name="possibleMaximumQuantity" value="<?php echo $possibleMaximumQuantity;?>">
+                                <div class="error"></div>
+                            </div>
                         </div>
                     </div>
                     <div class="row mb-3">
